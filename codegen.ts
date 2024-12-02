12f1f7c351e7f8172edfe2718975b3b26ec1e039
shubhswain45 <shubhkantswain@.com>
2024-12-02 10:42:40 +0530

    changing in design

--- codegen.ts
+++ codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://localhost:4000/graphql",
+  schema: "https://connectify-server-ef2a.onrender.com/graphql",
   documents: "**/*.{tsx,ts}",
   generates: {
     "gql/": {
